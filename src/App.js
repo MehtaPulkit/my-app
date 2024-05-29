4d7b5c30402fdcca1e40cc45a9e6024e9cb4f3fd
MehtaPulkit <mehta.pulkit94@gmail.com>
2024-05-29 16:26:27 +1000

    New Favicon

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Project from "./components/Project";
 import Resume from "./components/Resume";
 import Home from "./components/Home";
 import ErrorPage from "./components/ErrorPage";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
+if(process.env.NODE_ENV==="production") disableReactDevTools()
+  
 function App() {
   return (
     <div className="bg-white dark:bg-zinc-800">
